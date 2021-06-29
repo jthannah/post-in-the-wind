4b41bb74f9e62f04b0b61b6c125fa713a276efe2
Hannah, Joey <JHannah@globalspec.com>
2021-06-29 08:57:48 -0400

    test netfligy auto deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Pages from 'vite-plugin-pages'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  resolve: {
+    // Found solution here if there are issues in the future: https://github.com/vitejs/vite/issues/279
+    alias: { '@': path.resolve(__dirname, 'src') },
+  },
+  plugins: [
+    vue(),
+    Pages({
+      nuxtStyle: true,
+      routeBlockLang: 'yaml',
+      importMode: 'async',
+    }),
+  ],
+  build: {
+    sourcemap: true,
+  },
 })
